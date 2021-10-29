01f7508fcec4b82efc3c38011feb848ed3e2ebc0
Theomat <theomatricon@gmail.com>
2021-10-29 20:25:19 +0200

    Added distance function

--- pseas/instance_selection/udd.py
+++ pseas/instance_selection/udd.py
@@ -1,7 +1,7 @@
 from typing import Callable, Tuple, List, Optional
 import numpy as np
 from pseas.instance_selection.instance_selection import InstanceSelection
-
+from scipy import optimize
 def __compute_distance_matrix__(features: np.ndarray, distance: Callable[[np.ndarray, np.ndarray], float]) -> np.ndarray:
     """
     Computes the distance matrix between the instances.
@@ -28,6 +28,30 @@ def __compute_distance_matrix__(features: np.ndarray, distance: Callable[[np.nda
     return distance_matrix
 
 
+def __find_weights__(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    instances: int = x.shape[0]
+    features: int = x.shape[1]
+
+    qty: int = int(instances * (instances - 1) / 2)
+
+    dx: np.ndarray = np.zeros((qty, features))
+    dy: np.ndarray = np.zeros((qty,))
+
+    # Compute dataset
+    index: int = 0
+    for i in range(instances):
+        for j in range(i + 1, instances):
+            dx[index] = x[i] - x[j]
+            dy[index] = y[i] - y[j]
+            index += 1
+    np.square(dx, out=dx)
+    np.abs(dy, out=dy)
+    # np.square(dy, out=dy)
+
+    # weights = argmin_w_i (norm [w_i (x_i -x'_i)]_i - |y - y'|)^2
+    weights, residual = optimize.nnls(dx, dy)
+    return np.sqrt(weights)
+
 class UDD(InstanceSelection):
     """
     Uncertainty + alpha * Density - beta *  Diversity
@@ -36,8 +60,6 @@ class UDD(InstanceSelection):
 
     k: int - number of neighbours for density step
 
-    TODO: 
-    - missing self.distance
     """
 
     def __init__(self, alpha: float = 1, beta: float = 1, k : int = 5) -> None:
@@ -46,11 +68,17 @@ class UDD(InstanceSelection):
         self.beta: float = beta
         self.k : int = k
 
-    def ready(self, model, features, challenger_configuration, **kwargs) -> None:
+    def _dynamic_distance(self, x1: np.ndarray, x2: np.ndarray) -> float:
+        return np.linalg.norm(self._weights * (x1 - x2))
+
+    def ready(self, model, features, challenger_configuration, perf_matrix, **kwargs) -> None:
         self.model = model
         self.challenger_configuration = challenger_configuration
-        self._distances = __compute_distance_matrix__(features, self.distance)
         self.n_instances: int = self.features.shape[0]
+        # Find optimal distance function
+        y: np.ndarray = np.median(perf_matrix, axis=1)
+        self._weights: np.ndarray = __find_weights__(features, y)
+        self._distances = __compute_distance_matrix__(features, self._dynamic_distance)
 
     def reset(self) -> None:
         """
@@ -121,6 +149,7 @@ class UDD(InstanceSelection):
         for i in range(self.n_instances):
             if state[0][i] is not None:
                 scores[i] = 1e30
+        # How does that make sense?
         self._next = np.argmin(scores)
 
 
