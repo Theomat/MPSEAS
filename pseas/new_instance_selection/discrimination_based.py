f7f18ca81051a4e89a2465a3ad73b8558d2436dc
Theomat <theomatricon@gmail.com>
2021-12-02 16:32:52 +0100

    Fix

--- pseas/new_instance_selection/discrimination_based.py
+++ pseas/new_instance_selection/discrimination_based.py
@@ -18,7 +18,7 @@ class Discrimination(NewInstanceSelection):
         best_instance = 0
         for i in selectables_instances:
             times = predicted_perf_matrix[i, current_configurations]
-            std = np.count_nonzero(times > np.repeat(self._rho * np.min(times), times.shape[0]))
+            std = np.count_nonzero(times > np.repeat(self.rho * np.min(times), times.shape[0]))
             median = np.median(predicted_perf_matrix[i, current_configurations])
             if std / median > best_ratio:
                 best_instance = i
